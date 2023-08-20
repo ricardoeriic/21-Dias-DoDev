b2bedead021f4396d4e54f186a4f5d388d535ac0
Eric Ricardo <contato.ericricardo@gmail.com>
2023-08-20 09:41:52 -0300

    Exercícios do dia 6

---
+++ Dia 6/ex1.js
@@ -0,0 +1,11 @@
+console.log("Vamos começar Aumentando")
+
+for (let contador = 0; contador <= 50; contador +=5 )
+    console.log(contador)
+    console.log("Fim!")
+
+console.log ("Agora Diminuindo")
+
+for(let contador = 50; contador >=0; contador -= 5)
+    console.log(contador)
+    console.log("Fim!")
---
+++ Dia 6/ex2.js
@@ -0,0 +1,20 @@
+let tabuada = Number(prompt("Insira o número da tabuada que deseja, te devolverei ela e as próximas duas"))
+
+console.log("Aqui está a tabuada do " + tabuada)
+
+for (contador = 0; contador <= 10; contador++)
+    console.log(tabuada + " x " + contador + " = " + tabuada*contador)
+
+tabuada = tabuada + 1
+
+console.log("Aqui está a tabuada do " + tabuada)
+
+for (contador = 0; contador <= 10; contador++)
+    console.log(tabuada + " x " + contador + " = " + tabuada*contador)
+
+tabuada = tabuada + 1
+
+console.log("Aqui está a tabuada do " + tabuada)
+
+for (contador = 0; contador <= 10; contador++)
+    console.log(tabuada + " x " + contador + " = " + tabuada*contador)
